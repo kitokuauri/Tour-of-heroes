1851fe41e50c61cb20abb4d6c276b9d99d35c3c4
Auri <auri.suarezm@gmail.com>
2023-11-22 18:04:54 +0100

    Lesson 04F

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,20 +10,25 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes: Hero[] = [];
   selectedHero?: Hero;
-
-  onSelect(hero: Hero):void{
-    this.selectedHero=hero;
-  }
-
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+  heroes: Hero[] = [];
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) {}
+  onSelect(hero: Hero):void{
+    this.selectedHero=hero;
+    this.messageService.add(`HeroesComponent: Selected hero 1=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes=heroes);
+  }
+
+  
+
+  
 }
